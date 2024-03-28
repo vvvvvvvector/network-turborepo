1bdb79c2a5b047f1353104cee0cdcb12914421e6
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-03-29 02:25:25 +0300

    refactor(web): friends layout

---
+++ apps/web/components/friends/friends-or-requests-nav.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+import { PAGES } from '@/lib/constants';
+
+export const FriendsOrRequestsNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="rounded-lg bg-background p-5">
+      <ul className="flex flex-col gap-2">
+        <Link
+          href={{
+            pathname: PAGES.FRIENDS,
+            query: {
+              tab: 'all'
+            }
+          }}
+        >
+          <li
+            className={cn(
+              'cursor-pointer rounded p-2 text-sm transition-[background-color] hover:bg-accent',
+              {
+                'bg-accent': pathname === PAGES.FRIENDS
+              }
+            )}
+          >
+            My friends
+          </li>
+        </Link>
+        <Link
+          href={{
+            pathname: PAGES.FRIENDS_REQUESTS,
+            query: {
+              type: 'incoming'
+            }
+          }}
+        >
+          <li
+            className={cn(
+              'cursor-pointer rounded p-2 text-sm transition-[background-color] hover:bg-accent',
+              {
+                'bg-accent': pathname === PAGES.FRIENDS_REQUESTS
+              }
+            )}
+          >
+            Friend requests
+          </li>
+        </Link>
+      </ul>
+    </div>
+  );
+};
