2c14a98356ff782bec725617e34b4b4d64ee528a
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-04-27 16:25:32 +0200

    refactor: use edge runtime

--- apps/web/app/api/cron/route.ts
+++ apps/web/app/api/cron/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export async function GET() {
-  console.log('hell owrodl!!!!');
+  console.log('Hello world!');
 
   return NextResponse.json({ ok: true });
 }
