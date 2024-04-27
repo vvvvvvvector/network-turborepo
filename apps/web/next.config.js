6c3fc0d8ab711e249e293f5a587377df7ddb166d
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-04-27 16:16:33 +0200

    add test cron job

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  crons: [
+    {
+      path: '/api/cron',
+      schedule: '0 0/1 * 1/1 * ? *'
+    }
+  ]
 };
 
 module.exports = nextConfig;
