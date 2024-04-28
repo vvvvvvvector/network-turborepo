e1ac45275fda676d5bbb678dc0bf3b5c191490ad
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-04-28 13:38:05 +0200

    fix(web): protect messenger/[chatId] route

--- apps/web/app/(authorised)/messenger/[chatId]/page.tsx
+++ apps/web/app/(authorised)/messenger/[chatId]/page.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next';
 
 import { getChatData } from '@/app/(authorised)/messenger/api';
+import { auth } from '@/app/(auth)/auth';
 
 import { Chat } from '@/components/messenger/chat';
 
@@ -15,6 +16,8 @@ export const metadata: Metadata = {
 };
 
 export default async function ChatPage({ params }: Props) {
+  await auth();
+
   return (
     <div className="flex h-[calc(100vh-3.5rem-0.8rem-0.8rem)] flex-col gap-2 rounded-lg bg-background p-4">
       <Chat chat={await getChatData(params.chatId)} />
