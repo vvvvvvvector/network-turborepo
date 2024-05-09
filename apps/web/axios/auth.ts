79ae78c14ec443508c46c3346215d60b6f5d6dab
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-05-10 02:14:33 +0300

    testing..

--- apps/web/axios/auth.ts
+++ apps/web/axios/auth.ts
@@ -4,7 +4,7 @@ import { axiosApiInstance } from "@/axios";
 
 const signIn = async (values: { username: string; password: string }) => {
   const { data } = await axiosApiInstance.post<{ token: string }>(
-    "/auth/signin",
+    "/auth/sign-in",
     values
   );
 
@@ -18,7 +18,7 @@ const signUp = async (values: {
 }) => {
   const { data } = await axiosApiInstance.post<{
     receiver: string;
-  }>("/auth/signup", values);
+  }>("/auth/sign-up", values);
 
   return data;
 };
